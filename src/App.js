6646dca3020a39dab9377168a230f6b108dcc246
Lucas Bonini <lucas.bonini@cowmed.com.br>
2020-02-10 16:15:27 -0300

    searching robots

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots';
+
+
+
+
+class App extends React.Component {
+
+    constructor(){
+        super()
+            this.state = {
+            robots: robots,
+            searchfield: ''
+        
+        }
+    }
+
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value})
+        console.log(event.target.value);
+
+    }
+
+
+
+
+    render() {
+        
+        
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+
+
+        return(
+            <div className='tc'>
+                <h1>ROBOFRIENDS</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+            </div>
+        )  
+
+
+    }
+};
+
+export default App;
---
+++ src/SearchBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+const SearchBox = ({ searchfield, searchChange}) => {
+    return(
+        <div className='pa2'>
+
+        <input
+        type='search' 
+        placeholder='search robot' 
+        className='pa3 ba b--green bg-lightest-blue'
+        onChange={searchChange}
+        />
+        </div>
+    )
+};
+
+export default SearchBox;
